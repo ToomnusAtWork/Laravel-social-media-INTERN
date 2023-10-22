c9cc75d7a410df4629fee47954523635cc0f423e
ToomnusAtWork <131127987+ToomnusAtWork@users.noreply.github.com>
2023-10-22 22:40:05 +0700

    Change main page

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -11,7 +11,6 @@ const props = defineProps({
         type: Boolean,
     },
 });
-// fix bg color change.
 const classes = computed(() =>
     props.active
         ? 'inline-flex items-center px-1 pt-1 border-b-2 bg-zinc-50 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
